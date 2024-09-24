594fe558733c810172fe2151d36a5d8f4b64771c
Rudra Sharma <138853785+RudraSharma3@users.noreply.github.com>
2024-09-24 08:27:29 +0530

    Time: 29 ms (36.42%) | Memory: 29.9 MB (99.70%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //int nums[] = [-1,0,3,5,9,12];
         int low = 0;
         int mid;
         int high = nums.size()-1;
